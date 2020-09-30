09aec4afa7c066a4264531a2f14a57aa48e4af73
Keun Hee Lee <kenleeaws@gmail.com>
2020-09-30 17:49:59 +1000

    Create test make

---
+++ test_package/example3.py
@@ -0,0 +1,9 @@
+# example3.py
+
+import string_func
+
+some_string = "Hello, Universe!"
+
+print(string_func.stringLength(some_string))
+print(string_func.stringToLower(some_string))
+print(string_func.stringToUpper(some_string))
